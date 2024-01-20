ee223d5614cda6573faafe3966458c1d88314dc1
Aleksander <spjort2003@gmail.com>
2024-01-20 15:27:25 +0100

    uh

---
+++ src/main/java/org/example/gui/GameScreen.java
@@ -0,0 +1,17 @@
+package org.example.gui;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+public class GameScreen {
+	
+	private JPanel screenPanel;
+	
+	private BoardPane boardPane;
+	
+	
+
+}
